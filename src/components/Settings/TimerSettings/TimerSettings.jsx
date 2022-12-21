571c65f3c80e43c15bc9f1382055ddccec3dd216
Tizios9999 <davide.santonocito@outlook.com>
2022-12-21 09:53:24 +0100

    first minutes timer setup

--- src/components/Settings/TimerSettings/TimerSettings.jsx
+++ src/components/Settings/TimerSettings/TimerSettings.jsx
@@ -4,28 +4,49 @@ import { useContext } from 'react'
 
 export default function TimerSettings() {
 
+    const [appState, setAppState] = useContext(AppContext);
+
+    function handleChange(e) {
+
+        setAppState(prevState => {
+          return {
+            ...prevState,
+            timerSeconds: e.target.value * 60
+          };
+        })
+      }
+
+
     return (
 
     <div className={styles["timer--settings"]}>
-
+    
+      <div>
         <label htmlFor="input-minutes">
             Minutes: 
         </label>
         
         <input 
             id="input-minutes"
-            name="input-minutes" 
+            name="input-minutes"
+            value={appState.timerSeconds/60}
+            onChange={handleChange}
             className={styles["input-minutes"]}/>
+        
+      </div>
 
+      <div>
         <label htmlFor="input-countdown">
             Countdown seconds: 
         </label>
         
         <input 
             id="input-countdown"
-            name="input-countdown" 
+            name="input-countdown"
             className={styles["input-countdown"]}/>
+      </div>
 
         </div>
+
         )
 }
